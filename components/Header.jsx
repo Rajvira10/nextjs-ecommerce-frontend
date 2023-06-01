708511c2decd02f77e0da2ed791f767321b3ed6d
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-01 22:43:19 +0600

    connected to mongodb

--- components/Header.jsx
+++ components/Header.jsx
@@ -34,13 +34,13 @@ const Header = () => {
       <Center>
         <Wrapper>
           <Logo href="/">Ecommerce</Logo>
-          <nav>
+          <StyledNav>
             <NavLink href={"/"}>Home</NavLink>
             <NavLink href={"/products"}>All Products</NavLink>
             <NavLink href={"/categories"}>Categories</NavLink>
             <NavLink href={"/account"}>Account</NavLink>
             <NavLink href={"/cart"}>Cart (0)</NavLink>
-          </nav>
+          </StyledNav>
         </Wrapper>
       </Center>
     </StyledHeader>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Featured from "@/components/Featured";
 import Header from "@/components/Header";
 
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Header/>
