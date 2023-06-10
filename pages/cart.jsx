409d467d795e1f9c4c6a2ba221992147deb2f8c7
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2023-06-10 18:56:14 +0600

    working on the cart page

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,5 +1,12 @@
 import Header from "@/components/Header";
 import React from "react";
+import styled from "styled-components";
+
+const ColumnsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1.3fr 0.7fr;
+  gap: 40px;
+`;
 
 const CartPage = () => {
   return (
